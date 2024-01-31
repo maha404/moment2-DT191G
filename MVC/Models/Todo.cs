978177c219f6d77a61a882f1270bc4229e771111
maha404 <maria_halvarsson@hotmail.se>
2024-01-31 18:56:47 +0100

    Lade till validering.

--- MVC/Models/Todo.cs
+++ MVC/Models/Todo.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace moment2.Model;
 
 public class Todo 
 {
     // public int Id { get; set; }
+    [Required(ErrorMessage ="Fältet får inte vara tomt!")]
     public string? Task { get; set; }
     // public bool Status { get; set; }
 
