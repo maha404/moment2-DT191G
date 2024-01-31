950ed788b92dff056a066cc2d8573831a0c71a41
Your Name <you@example.com>
2024-01-31 20:33:06 +0100

    Rensade koden.

--- MVC/Models/Todo.cs
+++ MVC/Models/Todo.cs
@@ -4,10 +4,8 @@ namespace moment2.Model;
 
 public class Todo 
 {
-    // public int Id { get; set; }
     [Required(ErrorMessage ="Fältet får inte vara tomt!")]
     public string? Task { get; set; }
-    // public bool Status { get; set; }
 
     public Todo() {}
     public Todo(string ts)
